ae61982a1eb378d62635e504ceaa7b197c771894
programmaman <alexbraynen@hotmail.com>
2023-02-05 19:37:35 -0500

    Update hyponym_hypernym.py

--- foundations/hyponym_hypernym.py
+++ foundations/hyponym_hypernym.py
@@ -1,35 +1,89 @@
-# Levenshtein Edit Distance
-def problem2(string1, string2):
-    # Create Table
-    row = len(string1) + 1
-    column = len(string2) + 1
-    table = []
+import re
 
-    for i in range(row):
-        col = []
-        for j in range(column):
-            col.append(0)
-        table.append(col)
+# TODO: This implentation needs to be reinterpreted into OOP/
+# TODO: /structure to allow for more powerful meaning inferences
 
-    # Put string Length in Rows
-    for i in range(1, row):
-        table[i][0] = i
-    # Put string length in column
-    for j in range(1, column):
-        table[0][j] = j
+# Extract Hyper and Hypo Relationships
+def problem1(noun_list, corpus):
 
-    for i in range(1, row):
-        for j in range(1, column):
-            if string1[i - 1] == string2[j - 1]:
-                substitution_cost = 0
-            else:
-                substitution_cost = 2
-            # Fill table with edit distances
-            table[i][j] = min(table[i - 1][j] + 1,
-                              table[i][j - 1] + 1,
-                              table[i - 1][j - 1] + substitution_cost)
+    # Lower the case of all text, remove all formatting and remove unnecessary "a", "an", "the"
+    corpus = regularize_text(corpus)
 
-    return table[row - 1][column - 1]
+    # Add symbols for identifying meaningful segments
+    corpus = symbolize_text(corpus)
+
+    # Tokenize string into words
+    tokenized_corpus = corpus.split()
+
+    # Static hypernym to store parent hypernym when finding the meaning of lists of hyponyms
+    static_hypernym = ''
+
+    # Store index to capture list of hyponyms
+    list_beginning_index = 0
+
+    # Empty set to hold tuples
+    answer = set()
+
+    # Iterate through tokens until a symbol, "=", ">" or "+" is found
+    for iterator in range(len(tokenized_corpus)):
+
+        # if '=' is found, this is a "hyponym is hypernym" relationship.
+        if tokenized_corpus[iterator] == '=':
+            test_hyponym = tokenized_corpus[iterator - 1]
+            test_hypernym = tokenized_corpus[iterator + 1]
+            hyponym = ''
+            hypernym = ''
+            # check if token is in the NP list and replace it with NP list token if so
+            for i in range(len(noun_list)):
+                if test_hyponym in noun_list[i]:
+                    hyponym = noun_list[i]
+                if test_hypernym in noun_list[i]:
+                    hypernym = noun_list[i]
+            if len(hyponym) != 0 and len(hypernym) != 0:
+                hypo_tuple = (hypernym, hyponym)
+                answer.add(hypo_tuple)
+
+        # if '>' is found, this is a "hypernym such as hyponym" relationship.
+        if tokenized_corpus[iterator] == '>':
+            list_beginning_index = iterator
+            test_hypernym = tokenized_corpus[iterator - 1]
+            test_hyponym = tokenized_corpus[iterator + 1]
+            hyponym = ''
+            hypernym = ''
+
+            # check if token is in the NP list and replace it with NP list token if so
+            for i in range(len(noun_list)):
+                if test_hyponym in noun_list[i]:
+                    hyponym = noun_list[i]
+                if test_hypernym in noun_list[i]:
+                    hypernym = noun_list[i]
+                    static_hypernym = test_hypernym
+            if len(hyponym) != 0 and len(hypernym) != 0:
+                hypo_tuple = (hypernym, hyponym)
+                answer.add(hypo_tuple)
+
+        # if '+' is found, this is an additional "hypernym such as hyponym" relationship. Present these as tuples.
+        if tokenized_corpus[iterator] == '+':
+            list_ending_index = iterator
+            test_hyponym = tokenized_corpus[iterator + 1]
+            hyponym = ''
+
+            # check if token is in the NP list and replace it with NP list token if so
+            for i in range(len(noun_list)):
+                if test_hyponym in noun_list[i]:
+                    hyponym = noun_list[i]
+            if len(hyponym) != 0 and len(static_hypernym) != 0:
+                hypo_tuple = (static_hypernym, hyponym)
+                answer.add(hypo_tuple)
+            while list_beginning_index < list_ending_index:
+                for i in range(len(noun_list)):
+                    if tokenized_corpus[list_beginning_index] in noun_list[i]:
+                        hyponym = noun_list[i]
+                        hypo_tuple = (static_hypernym, hyponym)
+                        answer.add(hypo_tuple)
+                        break
+                list_beginning_index = list_beginning_index + 1
+    return answer
 
 
 # Lower case of all text, remove all formatting except whitespace and remove standalone "a", "an", "the"
